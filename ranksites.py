1bea7f2632d0679b2c2af9ccad58a0661f0a66c4
Matthew Meinzer <meinzer.matthew@gmail.com>
2017-05-06 21:04:11 -0500

    saves ratings

--- ranksites.py
+++ ranksites.py
@@ -4,12 +4,15 @@ from selenium.webdriver.common.keys import Keys
 def main():
     with open("site_urls.txt") as f:
         urls = [line.strip() for line in f]
-        print(urls)
-    driver = webdriver.Chrome("/home/matt/Downloads/chromedriver")
-    for url in urls:
-        driver.get(url)
-        rating = raw_input("Rate this site (1-3): ")
-        print(rating)
+    with open("site_ratings.txt", 'w') as w:
+        driver = webdriver.Chrome("/home/matt/Downloads/chromedriver")
+        for url in urls:
+            driver.get(url)
+            value = raw_input("Rate this site (1-3): ")
+            if value in ["1", "2", "3"]:
+                w.write(value + "\n")
+            else:
+                break
 
 if __name__ == '__main__':
     main()
